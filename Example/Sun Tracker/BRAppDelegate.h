4c0e0084695df1155b08797efc2ac0a714b7e922
Julien Ducret <julien@agencymobile.co.uk>
2014-04-09 21:53:19 +0100

    Added Cocoapods integration

---
+++ Example/Sun Tracker/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  Sun Tracker
+//
+//  Created by Julien Ducret on 03/02/2014.
+//  Copyright (c) 2014 Julien Ducret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Sun Tracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sun Tracker
+//
+//  Created by Julien Ducret on 03/02/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
